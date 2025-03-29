4194b98782ff449e1cd3a75fa13e4466287c6a28
Masaya Iwasa <miwasa@c6r2s3.42tokyo.jp>
2025-03-29 22:31:57 +0900

    [fix]

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -3,49 +3,42 @@
 PmergeMe::PmergeMe() : _vector_comparisons(0), _list_comparisons(0) {}
 
 PmergeMe::PmergeMe(int argc, char *argv[]) : _vector_comparisons(0), _list_comparisons(0) {
-    
-    std::cout << "Before: ";
-    for (int i = 1; i < argc; ++i) {
-        std::cout << argv[i] << " ";
-    }
-    std::cout << std::endl;
-    
-    
-    for (int i = 1; i < argc; ++i) {
-        char *endptr;
-        long value = std::strtol(argv[i], &endptr, 10);
-        
-        
-        if (*endptr != '\0' || value < 0) {
-            throw std::runtime_error("Error: Invalid input. Only positive integers are allowed.");
-        }
-        
-        
-        _vector.push_back(static_cast<unsigned int>(value));
-        _list.push_back(static_cast<unsigned int>(value));
-    }
-    
-    
-    clock_t start_vector = clock();
-    vectorSort();
-    clock_t end_vector = clock();
-    
-    clock_t start_list = clock();
-    listSort();
-    clock_t end_list = clock();
-    
-    
-    double time_vector = static_cast<double>(end_vector - start_vector) / CLOCKS_PER_SEC * 1000000;
-    double time_list = static_cast<double>(end_list - start_list) / CLOCKS_PER_SEC * 1000000;
-    
-    std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector : " 
-              << time_vector << " us" << std::endl;
-    std::cout << "Time to process a range of " << _list.size() << " elements with std::list : " 
-              << time_list << " us" << std::endl;
-    
-    
-    std::cout << "Number of comparisons in std::vector: " << _vector_comparisons << std::endl;
-    std::cout << "Number of comparisons in std::list: " << _list_comparisons << std::endl;
+	std::cout << "Before: ";
+	for (int i = 1; i < argc; ++i) {
+		std::cout << argv[i] << " ";
+	}
+	std::cout << std::endl;
+
+	for (int i = 1; i < argc; ++i) {
+		char *endptr;
+		long value = std::strtol(argv[i], &endptr, 10);
+		
+		if (*endptr != '\0' || value < 0) {
+			throw std::runtime_error("Error: Invalid input. Only positive integers are allowed.");
+		}
+		_vector.push_back(static_cast<unsigned int>(value));
+		_list.push_back(static_cast<unsigned int>(value));
+	}
+
+
+	clock_t start_vector = clock();
+	vectorSort();
+	clock_t end_vector = clock();
+
+	clock_t start_list = clock();
+	listSort();
+	clock_t end_list = clock();
+
+	double time_vector = static_cast<double>(end_vector - start_vector) / CLOCKS_PER_SEC * 1000000;
+	double time_list = static_cast<double>(end_list - start_list) / CLOCKS_PER_SEC * 1000000;
+
+	std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector : " 
+				<< time_vector << " us" << std::endl;
+	std::cout << "Time to process a range of " << _list.size() << " elements with std::list : " 
+				<< time_list << " us" << std::endl;
+
+	std::cout << "Number of comparisons in std::vector: " << _vector_comparisons << std::endl;
+	std::cout << "Number of comparisons in std::list: " << _list_comparisons << std::endl;
 }
 
 PmergeMe::PmergeMe(const PmergeMe &other) {
@@ -56,48 +49,36 @@ PmergeMe::~PmergeMe() {
 }
 
 PmergeMe& PmergeMe::operator=(const PmergeMe &other) {
-    if (this != &other) {
-        _vector = other._vector;
-        _list = other._list;
-        _vector_comparisons = other._vector_comparisons;
-        _list_comparisons = other._list_comparisons;
-    }
-    return *this;
+	if (this != &other) {
+		_vector = other._vector;
+		_list = other._list;
+		_vector_comparisons = other._vector_comparisons;
+		_list_comparisons = other._list_comparisons;
+	}
+	return *this;
 }
 
 
 void PmergeMe::display() {
-    
-    std::cout << "Vector: ";
-    for (std::vector<unsigned int>::iterator it = _vector.begin(); it != _vector.end(); ++it) {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-    
-    
-    std::cout << "List: ";
-    for (std::list<unsigned int>::iterator it = _list.begin(); it != _list.end(); ++it) {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-}
+	std::cout << "Vector: ";
+	for (std::vector<unsigned int>::iterator it = _vector.begin(); it != _vector.end(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 
-
-void PmergeMe::display(std::vector<pairs> out) {
-	std::cout << "After:  ";
-	for (std::vector<pairs>::iterator it = out.begin(); it != out.end(); it++) {
-		std::cout << (*it).num << " ";
+	std::cout << "List: ";
+	for (std::list<unsigned int>::iterator it = _list.begin(); it != _list.end(); ++it) {
+		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
 }
 
-
 size_t PmergeMe::jacobsthal(size_t n) {
-    return (std::pow(2, n + 1) + std::pow(-1, n) * 2) / 3;
+	return (std::pow(2, n + 1) + std::pow(-1, n) * 2) / 3;
 }
 
 
-size_t PmergeMe::BS_vec(std::vector<pairs> res, size_t right, int value) {
+size_t PmergeMe::BinarySearch_vector(std::vector<pairs> res, size_t right, int value) {
 	size_t left = 0, mid;
 
 	while (left <= right) {
@@ -118,7 +99,6 @@ size_t PmergeMe::BS_vec(std::vector<pairs> res, size_t right, int value) {
 	}
 	return left;
 }
-
 
 std::vector<pairs> PmergeMe::vector_sort(std::vector<pairs> arr) {
 	std::vector<pairs> res, big, small;
@@ -163,11 +143,11 @@ std::vector<pairs> PmergeMe::vector_sort(std::vector<pairs> arr) {
 		if (groupsize > small.size())
 			groupsize = small.size();
 		for (size_t i = 0; i < groupsize; i++) {
-			size_t	BS_point = groupsize - i - 1;
-			size_t	right_side = 1 + group_passed * 2 + BS_point + i;
-			size_t	insert_point = BS_vec(res, right_side, small[BS_point].num);
-			res.insert(res.begin() + insert_point, small[BS_point]);
-			small.erase(small.begin() + BS_point);
+			size_t	reverseIndex = groupsize - i - 1;
+			size_t	right_side = 1 + group_passed * 2 + reverseIndex + i;
+			size_t	insert_point = BinarySearch_vector(res, right_side, small[reverseIndex].num);
+			res.insert(res.begin() + insert_point, small[reverseIndex]);
+			small.erase(small.begin() + reverseIndex);
 		}
 		group_passed += groupsize;
 		count++;
@@ -191,193 +171,149 @@ void PmergeMe::vectorSort() {
 }
 
 
-size_t PmergeMe::BS_list(std::list<graph> res, size_t right, int value) {
-    size_t left = 0, mid;
-    
-    while (left <= right) {
-        mid = left + (right - left) / 2;
-        
-        std::list<graph>::iterator it = res.begin();
-        std::advance(it, mid);
-        
-        _list_comparisons++;
-        if (it->num == value) {
-            return mid;
-        }
-        
-        if (it->num < value) {
-            if (mid == right)
-                return mid + 1;
-            left = mid + 1;
-        } else {
-            if (mid == 0)
-                return 0;
-            right = mid - 1;
-        }
-    }
-    return left;
+size_t PmergeMe::BinarySearch_list(std::list<graph> res, size_t right, int value) {
+	size_t left = 0, mid;
+
+	while (left <= right) {
+		mid = left + (right - left) / 2;
+		
+		std::list<graph>::iterator it = res.begin();
+		std::advance(it, mid);
+		
+		_list_comparisons++;
+		if (it->num == value) {
+			return mid;
+		}
+		
+		if (it->num < value) {
+			if (mid == right)
+				return mid + 1;
+			left = mid + 1;
+		} else {
+			if (mid == 0)
+				return 0;
+			right = mid - 1;
+		}
+	}
+	return left;
 }
-std::list<graph> PmergeMe::list_sort(std::list<graph> arr)
+std::list<graph> PmergeMe::list_sort(std::list<graph> lis)
 {
-    
-    if (arr.size() == 1)
-        return arr;
-    else if (arr.size() == 2)
-    {
-        _list_comparisons++;
-        
-        std::list<graph>::iterator it = arr.begin();
-        std::list<graph>::iterator jt = it;
-        ++jt;
-        if (it->num > jt->num)
-        {
-            
-            graph temp = *it;
-            *it = *jt;
-            *jt = temp;
-        }
-        return arr;
-    }
+	if (lis.size() == 1)
+		return lis;
+	else if (lis.size() == 2)
+	{
+		_list_comparisons++;
+		std::list<graph>::iterator it = lis.begin();
+		std::list<graph>::iterator jt = it;
+		++jt;
+		if (it->num > jt->num)
+		{
+			graph temp = *it;
+			*it = *jt;
+			*jt = temp;
+		}
+		return lis;
+	}
 
-    
-    
-    
-    std::list<graph> big;
-    {
-        
-        std::list<graph>::iterator it = arr.begin();
-        while (it != arr.end()) 
-        {
-            std::list<graph>::iterator jt = it;
-            ++jt;
-            if (jt == arr.end())
-            {
-                
-                break;
-            }
-            
-            _list_comparisons++;
-            if (it->num > jt->num)
-            {
-                it->p.push_back(*jt); 
-                big.push_back(*it);
-            }
-            else
-            {
-                jt->p.push_back(*it); 
-                big.push_back(*jt);
-            }
-            
-            ++it; 
-            ++it;
-        }
-    }
+	std::list<graph> big;
+	{
+		std::list<graph>::iterator it = lis.begin();
+		while (it != lis.end()) 
+		{
+			std::list<graph>::iterator jt = it;
+			++jt;
+			if (jt == lis.end())
+			{
+				break;
+			}
+			_list_comparisons++;
+			if (it->num > jt->num)
+			{
+				it->p.push_back(*jt); 
+				big.push_back(*it);
+			}
+			else
+			{
+				jt->p.push_back(*it); 
+				big.push_back(*jt);
+			}
+			++it; 
+			++it;
+		}
+	}
 
-    
-    std::list<graph> res = list_sort(big);
+	std::list<graph> res = list_sort(big);
 
-    
-    if (!res.empty() && res.begin()->p.empty())
-        return res;
+	if (!res.empty() && res.begin()->p.empty())
+		return res;
+	std::list<graph> small;
+	{
+		for (std::list<graph>::iterator rit = res.begin(); rit != res.end(); ++rit)
+		{
+			graph last = rit->p.back();
+			rit->p.pop_back();
+			small.push_back(last);
+		}
+	}
 
-    
-    
-    
-    std::list<graph> small;
-    {
-        
-        
-        for (std::list<graph>::iterator rit = res.begin(); rit != res.end(); ++rit)
-        {
-            
-            graph last = rit->p.back();
-            rit->p.pop_back();
-            small.push_back(last);
-        }
-    }
+	if (lis.size() % 2 != 0)
+	{
+		graph leftover = lis.back();
+		small.push_back(leftover);
+	}
 
-    
-    if (arr.size() % 2 != 0)
-    {
-        
-        graph leftover = arr.back();
-        small.push_back(leftover);
-    }
+	size_t count         = 1;
+	size_t group_passed  = 0;
 
-    
-    
-    
-    size_t count         = 1;
-    size_t group_passed  = 0;
-    
-    
-    
-    if (!small.empty())
-    {
-        
-        res.insert(res.begin(), small.front());
-        small.pop_front();
-    }
+	if (!small.empty())
+	{
+		res.insert(res.begin(), small.front());
+		small.pop_front();
+	}
 
-    
-    while (!small.empty())
-    {
-        size_t groupsize = jacobsthal(count);
-        if (groupsize > small.size())
-            groupsize = small.size();
+	while (!small.empty())
+	{
+		size_t groupsize = jacobsthal(count);
+		if (groupsize > small.size())
+			groupsize = small.size();
 
-        
-        for (size_t i = 0; i < groupsize; ++i)
-        {
-            
-            
-            
-            
-            const size_t BS_point   = groupsize - i - 1;
-            const size_t right_side = 1 + group_passed*2 + BS_point + i;
+		for (size_t i = 0; i < groupsize; ++i)
+		{
+			const size_t reverseIndex   = groupsize - i - 1;
+			const size_t right_side = 1 + group_passed*2 + reverseIndex + i;
 
-            
-            
-            
-            
-            std::list<graph>::iterator st = small.begin();
-            for (size_t s = 0; s < BS_point; ++s)
-                ++st;
+			std::list<graph>::iterator st = small.begin();
+			for (size_t s = 0; s < reverseIndex; ++s)
+				++st;
 
-            graph val = *st; 
-            small.erase(st);
+			graph val = *st; 
+			small.erase(st);
 
-            
-            
-            
-            
-            size_t insert_index = BS_list(res, right_side, val.num);
+			size_t insert_index = BinarySearch_list(res, right_side, val.num);
 
-            
-            std::list<graph>::iterator ins = res.begin();
-            for (size_t j = 0; j < insert_index; ++j)
-                ++ins;
-
-            res.insert(ins, val);
-        }
-        group_passed += groupsize;
-        count++;
-    }
-
-    return res;
+			std::list<graph>::iterator ins = res.begin();
+			for (size_t j = 0; j < insert_index; ++j)
+				++ins;
+			res.insert(ins, val);
+		}
+		group_passed += groupsize;
+		count++;
+	}
+	return res;
 }
 
 void PmergeMe::listSort() {
-    std::list<graph> lis;
-    
-    for (std::list<unsigned int>::iterator it = _list.begin(); it != _list.end(); it++) {
-        graph p;
-        p.num = *it;
-        lis.push_back(p);
-    }
-    std::list<graph> sorted = list_sort(lis);
-    _list.clear();
-    for (std::list<graph>::iterator it = sorted.begin(); it != sorted.end(); it++) {
-        _list.push_back(static_cast<unsigned int>((*it).num));
-    }
+	std::list<graph> lis;
+
+	for (std::list<unsigned int>::iterator it = _list.begin(); it != _list.end(); it++) {
+		graph p;
+		p.num = *it;
+		lis.push_back(p);
+	}
+	std::list<graph> sorted = list_sort(lis);
+	_list.clear();
+	for (std::list<graph>::iterator it = sorted.begin(); it != sorted.end(); it++) {
+		_list.push_back(static_cast<unsigned int>((*it).num));
+	}
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -9,46 +9,40 @@
 #include <cmath>
 
 struct pairs {
-    double num;
-    std::vector<pairs> p;
+	double num;
+	std::vector<pairs> p;
 };
 
 struct graph {
-    double num;
-    std::list<graph> p;
+	double num;
+	std::list<graph> p;
 };
 
 class PmergeMe
 {
 private:
-    std::list<unsigned int> _list;
-    std::vector<unsigned int> _vector;
-    PmergeMe();
-    
-    // Comparison counters
-    size_t _vector_comparisons;
-    size_t _list_comparisons;
+	std::list<unsigned int> _list;
+	std::vector<unsigned int> _vector;
+	PmergeMe();
+	size_t _vector_comparisons;
+	size_t _list_comparisons;
     
 public:
-    PmergeMe(int argc,char *argv[]);
-    PmergeMe(const PmergeMe &other);
-    virtual ~PmergeMe();
-    PmergeMe& operator=(const PmergeMe &other);
-    
+	PmergeMe(int argc,char *argv[]);
+	PmergeMe(const PmergeMe &other);
+	virtual ~PmergeMe();
+	PmergeMe& operator=(const PmergeMe &other);
 
-    size_t jacobsthal(size_t n);
-    // Display methods
-    void display(); // General display method
-    void display(std::vector<pairs> out); // Helper method for vector display
-    
-    // Vector sort methods
-    void vectorSort();
-    std::vector<pairs> vector_sort(std::vector<pairs> arr);
-    size_t BS_vec(std::vector<pairs> res, size_t right, int value);
-    // size_t group_size(size_t n);
-    
-    // List sort methods
-    void listSort();
-    std::list<graph> list_sort(std::list<graph> arr);
-    size_t BS_list(std::list<graph> res, size_t right, int value);
+	size_t jacobsthal(size_t n);
+	void display();
+
+	// Vector sort methods
+	void vectorSort();
+	std::vector<pairs> vector_sort(std::vector<pairs> arr);
+	size_t BinarySearch_vector(std::vector<pairs> res, size_t right, int value);
+
+	// List sort methods
+	void listSort();
+	std::list<graph> list_sort(std::list<graph> arr);
+	size_t BinarySearch_list(std::list<graph> res, size_t right, int value);
 };
